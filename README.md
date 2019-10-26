1984c3db415efd8266ede265948d9b5112c18622
jerard <qbin221@gmail.com>
2019-10-26 13:13:33 -0400

    finished project, added readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # GiphyProject
+
+A jQuery project created during UCF's coding bootcamp
+
+## How to Use
+* Click on the corresponding buttons to display images of comedians from the GiphyAPI
+* Click on any image to animate the GIF, click again to reset it
+* Type the name of another comedian in the input and submit to create it's own button
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,23 +5,29 @@ $(document).ready(function () {
     const limit = 10;
     const rating = "g";
 
-
+    //Creates buttons from the topics array
     function createButtons() {
+
         $(".button-holder").empty();
 
         for (let i = 0; i < topics.length; i++) {
+
             let newButton = $("<button>");
-            newButton.addClass("search-button");
+            newButton.addClass("search-button btn btn-success");
             newButton.attr("value", topics[i]);
             newButton.html(topics[i]);
             newButton.appendTo(".button-holder");
+
         }
 
     }
 
+    //Initializing buttons in original array when page loads
     createButtons();
 
+    //Makes a request to GiphyAPI based on what button we click
     $(document).on("click", ".search-button", function () {
+
         let click = $(this).val();
 
         let queryURL = `https://api.giphy.com/v1/gifs/search?q=${click}&limit=${limit}&rating=${rating}&api_key=${apiKey}`
@@ -31,62 +37,55 @@ $(document).ready(function () {
             method: "GET"
 
         }).then(function (response) {
+
             $(".gif-holder").empty();
 
+            //Displays the first 10 search results
             for (let i = 0; i < limit; i++) {
+
+                //Creating new elements to house our search results and display to the page
                 let newDiv = $("<div>");
-                let newRating = $("<p>").html(response.data[i].rating);;
-                let newImage = $("<img>").attr("src", response.data[i].images.fixed_height_still.url);
+                let newRating = $("<p>").html(`Rating: ${response.data[i].rating.toUpperCase()}`);;
+                let newImage = $("<img>").attr({
+                    src: response.data[i].images.fixed_height_still.url,
+                    "data-still": response.data[i].images.fixed_height_still.url,
+                    "data-animate": response.data[i].images.fixed_height.url,
+                    "data-state": "still"
+                }).addClass("gif");
                 newDiv.append(newRating);
                 newDiv.append(newImage);
                 newDiv.appendTo(".gif-holder");
+
             }
         });
 
 
     });
 
+    //Adds in the input of the textbox to the array and creates a button of it
     $(".create-button").on("click", function () {
         event.preventDefault();
 
         topics.push($(".create-input").val());
         createButtons();
+
+        $(".create-input").val("");
     });
 
+    //Toggle a still image and animated GIF on click
+    $(document).on("click", ".gif", function () {
+
+        let state = $(this).attr("data-state");
+
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"))
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    });
 
 })
